6510dcb5ddd3ee8d348d0aefa4a7be27e4a5c1c8
cy77cc <483564813@qq.com>
2025-12-18 22:47:47 +0800

    feat(nacos): 添加服务实例更新功能
    - 新增 Update 方法支持动态更新 Nacos 服务实例
    - 自动获取本机 IP 和端口以简化配置
    - 支持设置实例权重、健康状态和元数据
    - 完善服务注册与注销逻辑的一致性

---
+++ test/test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"gateway/config"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cy77cc/hisshop/common/nacos"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load(".env")
+	cfg := config.Get()
+	cfg.Nacos.LoadNacosEnv()
+	instance, err := nacos.NewNacosInstance(cfg.Nacos)
+	if err != nil {
+		panic(err)
+	}
+
+	err = instance.Register("usercenter", "118.193.38.89", 443, map[string]string{"instance": "1"})
+	err = instance.Register("usercenter", "115.190.245.134", 8080, map[string]string{"instance": "2"})
+	err = instance.Register("usercenter", "115.190.245.134", 8848, map[string]string{"instance": "3"})
+	if err != nil {
+		panic(err)
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+}
